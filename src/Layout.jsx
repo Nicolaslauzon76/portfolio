a58f3875c539cb9f894015fdae8ca6f3d5de9862
Nic <1728062@cstj.qc.ca>
2023-12-15 00:12:51 -0500

    fix-layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+
 const Layout = () => {
   return (
     <div className="interface">
-        
+      {/* <Nav /> */}
 
-        
+      <main>
+        <Outlet />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
