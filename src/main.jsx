6de6b0cb7a9d7f0b2fabc063c8679f41284fd363
Nic <1728062@cstj.qc.ca>
2023-12-15 00:13:07 -0500

    fix-links

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./App";
 import "../src/assets/scss/main.scss";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
