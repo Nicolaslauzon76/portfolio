478f4fd1fcc646857328e1817fc1b63b7bb3e468
Nic <1728062@cstj.qc.ca>
2023-12-16 23:46:59 -0500

    photo-et-composant

---
+++ src/components/Moi/Moi.jsx
@@ -0,0 +1,18 @@
+import PhotoUn from "../../assets/img/png/photoun.jpg"
+import PhotoDeux from "../../assets/img/png/photodeux.jpg"
+import PhotoTrois from "../../assets/img/png/phototrois.jpg"
+
+const Moi = () => {
+  return (
+    <section className='moi'>
+        <div className="photos">
+        <div className="photos_un">
+
+        </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Moi
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import Header from "../components/Header/Header";
+
 const Home = () => {
   return (
     <div className="accueil_container">
-      <Header />
+
     </div>
   );
 };
