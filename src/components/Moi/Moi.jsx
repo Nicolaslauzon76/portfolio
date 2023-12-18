6c2507a2914574bce92641377f7e8e6f9b43cae1
Nic <1728062@cstj.qc.ca>
2023-12-18 11:20:18 -0500

    feature-section-apropos

--- src/components/Moi/Moi.jsx
+++ src/components/Moi/Moi.jsx
@@ -1,18 +1,52 @@
-import PhotoUn from "../../assets/img/png/photoun.jpg"
-import PhotoDeux from "../../assets/img/png/photodeux.jpg"
-import PhotoTrois from "../../assets/img/png/phototrois.jpg"
+import PhotoUn from "../../assets/img/png/photoun.jpg";
+import PhotoDeux from "../../assets/img/png/photodeux.jpg";
+import PhotoTrois from "../../assets/img/png/phototrois.jpg";
+import Boutons from "../Header/Boutons";
 
 const Moi = () => {
   return (
-    <section className='moi'>
-        <div className="photos">
+    <section className="moi">
+      <article className="apropos">
+        <h3>À propos</h3>
+        <span>de moi</span>
+
+        <p>
+          <span className="rouge">Passionné</span>,{" "}
+          <span className="rouge">enjoué</span> et{" "}
+          <span className="rouge">leader de nature</span>, je désire travailler
+          en Web pour mettre en valeur ma{" "}
+          <span className="rouge">créativité</span> et mes{" "}
+          <span className="rouge">compétences techniques</span>. Mon{" "}
+          <span className="rouge">engagement</span> et ma{" "}
+          <span className="rouge">capacité à motiver mon équipe</span> me
+          permettent d&apos;exceller dans les projets et de contribuer de
+          manière significative à la croissance et à l&apos;innovation de toute
+          entreprise.
+        </p>
+
+        <Boutons classe={"apropos__boutons"} />
+      </article>
+      <div className="photos">
         <div className="photos_un">
-
+          <div>
+            <img src={PhotoUn} alt="Nicolas Lauzon" />
+          </div>
         </div>
+
+        <div className="photos_deux">
+          <div>
+            <img src={PhotoDeux} alt="Nicolas Lauzon" />
+          </div>
         </div>
 
+        <div className="photos_trois">
+          <div>
+            <img src={PhotoTrois} alt="Nicolas Lauzon" />
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Moi
+export default Moi;
