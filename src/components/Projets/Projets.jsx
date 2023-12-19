dfa0b294936dd17d8e0fc098cac71e9496a37c1f
Nic <1728062@cstj.qc.ca>
2023-12-19 00:01:05 -0500

    feature-section-projets

--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -1,6 +1,37 @@
 const Projets = () => {
   return (
-    <div>Projets</div>
+    <section className="projets">
+        <div className="projets__titre">
+            <h3>Mes projets</h3>
+            <button>
+                <span className="rouge">Galerie</span>
+            </button>
+        </div>
+
+        <article className="apercu__galerie">
+            <div className="img1">
+                
+            </div>
+            <div className="img2">
+
+            </div>
+            <div className="img3">
+
+            </div>
+            <div className="img4">
+
+            </div>
+            <div className="img5">
+
+            </div>
+            <div className="img6">
+
+            </div>
+            <div className="img7">
+
+            </div>
+        </article>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Competences from "../components/Competences/Competences";
 import Moi from "../components/Moi/Moi";
+import Projets from "../components/Projets/Projets";
 
 const Home = () => {
   return (
     <div className="accueil_container">
       <Moi />
       <Competences />
-      
+      <Projets />
     </div>
   );
 };
