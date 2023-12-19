835d5b97302525d959f746dc59acf181ac2e8c9b
Nic <1728062@cstj.qc.ca>
2023-12-18 23:18:21 -0500

    feature-section-competences

---
+++ src/components/Competences/Competences.jsx
@@ -0,0 +1,87 @@
+import Logiciel from "../../assets/img/svg/logiciel.svg";
+
+const Competences = () => {
+  return (
+    <section className="competences">
+      <div className="competences_inner">
+        <h3>Mes compétences</h3>
+
+        <article>
+          <h4>Développement Front-End</h4>
+          <ul>
+            <li>
+              Travailler avec des frameworks front-end tels que{" "}
+              <span className="rouge">React</span> et{" "}
+              <span className="rouge">Vue</span>.
+            </li>
+
+            <li>
+              Assurer que l'interface utilisateur s'adapte à{" "}
+              <span className="rouge">différents appareils</span>
+              et <span className="rouge">tailles d'écran</span>.
+            </li>
+
+            <li>
+              Réaliser des maquettes <span className="rouge">UX / UI</span>.
+            </li>
+          </ul>
+        </article>
+
+        <article>
+          <h4>Conception graphique</h4>
+          <ul>
+            <li>
+              Concevoir des <span className="rouge">logos</span>, des{" "}
+              <span className="rouges">images</span> et des{" "}
+              <span className="rouge">icônes</span>.
+            </li>
+
+            <li>
+              Utiliser des outils de conception comme{" "}
+              <span className="rouge">Photoshop</span> et{" "}
+              <span className="rouge">Illustrator</span>.
+            </li>
+
+            <li>
+              Sélectionner soigneusement les{" "}
+              <span className="rouge">couleurs</span>, les{" "}
+              <span className="rouge">typographies</span> et les
+              <span className="rouge">formes</span>.
+            </li>
+          </ul>
+        </article>
+
+        <article>
+          <h4>Traiter les images fixes et en mouvements</h4>
+          <ul>
+            <li>
+              Réaliser la <span className="rouge">retouche</span>, le{" "}
+              <span className="rouge">recadrage</span> et la{" "}
+              <span className="rouge">correction</span> des couleurs sur des{" "}
+              <span className="rouge">photos</span>.
+            </li>
+
+            <li>
+              <span className="rouge">Animer</span> des éléments et générer des{" "}
+              <span className="rouge">mouvements</span> dans les{" "}
+              <span className="rouge">vidéos</span>.
+            </li>
+
+            <li>
+              Maîtriser la <span className="rouge">théorie</span> des couleurs,
+              les <span className="rouge">profils</span> de couleur et les
+              palettes pour assurer une{" "}
+              <span className="rouge">cohérence visuelle</span>.
+            </li>
+          </ul>
+        </article>
+
+      </div>
+        <div className="logiciels">
+          <img src={Logiciel} alt="" />
+        </div>
+    </section>
+  );
+};
+
+export default Competences;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Competences from "../components/Competences/Competences";
+import Moi from "../components/Moi/Moi";
 
 const Home = () => {
   return (
     <div className="accueil_container">
-
+      <Moi />
+      <Competences />
+      
     </div>
   );
 };
