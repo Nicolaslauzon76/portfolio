6493394d1904f033623103d67a8d4e0e912038e4
Nic <1728062@cstj.qc.ca>
2023-12-19 23:22:10 -0500

    fix

--- src/components/Moi/Moi.jsx
+++ src/components/Moi/Moi.jsx
@@ -1,6 +1,6 @@
 import PhotoUn from "../../assets/img/png/photoun.jpg";
 import PhotoDeux from "../../assets/img/png/photo.jpg";
-import PhotoTrois from "../../assets/img/png/phototrois.jpg";
+import PhotoTrois from "../../assets/img/png/image.jpg";
 import urlMusiqueBleuBlanc from "../../assets/img/svg/Musique_bleu_blanc.svg";
 
 import Boutons from "../Header/Boutons";
